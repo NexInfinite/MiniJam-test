11f49294bd42974c129a0bdfc9e319f984ebf50d
NexInfinite <stormparardiseonline@gmail.com>
2018-07-13 21:58:13 +0100

    Stable version 3.2, Movement is broken, but no crashes occur!

--- Mini jam competition/Levels/Enime.gd
+++ Mini jam competition/Levels/Enime.gd
@@ -2,7 +2,6 @@ extends RigidBody2D
 
 func _physics_process(delta):
 	var collisions = get_colliding_bodies()
-	print(collisions)
 	if 'Player' in collisions:
 		print('working')
 	pass
--- Mini jam competition/Levels/RigidBody2D.gd
+++ Mini jam competition/Levels/RigidBody2D.gd
@@ -1,31 +1,16 @@
 extends RigidBody2D
 
-const UP = Vector2(0, -1)
-const GRAVITY = 25
-const MAX_SPEED = 500
-const ACCELERATION = 80
-const JUMP_HEIGHT = -890
-var motion = Vector2()
+var movement = Vector2()
 
-func _physics_process(delta):
-	motion.y += GRAVITY
-	var friction = false
-	
+func get_inputs():
 	if Input.is_key_pressed(KEY_H):
-		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
-	elif Input.is_key_pressed(KEY_F):
-		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+		movement = Vector2(100, 0)
 	else:
-		friction = true
+		movement = Vector2()
+		
+func _process(delta):
+	get_inputs()
 	
-	if is_on_floor():
-		if Input.is_key_pressed(KEY_T):
-			motion.y = JUMP_HEIGHT
-		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.4)
-	else:
-		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.2)
-	
-	motion = move_and_slide(motion, UP)
+func _physics_process(delta):
+	add_force(Vector2(), movement)
 	pass
