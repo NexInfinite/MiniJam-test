fbcb868464b002c1dcc8bd9f1e57004eff207ded
NexInfinite <stormparardiseonline@gmail.com>
2018-07-13 22:29:50 +0100

    Unstable version 3.22 Still working on the movement. I have to go now. Have fun figuring it out!

--- Mini jam competition/Levels/RigidBody2D.gd
+++ Mini jam competition/Levels/RigidBody2D.gd
@@ -1,16 +1,16 @@
 extends RigidBody2D
 
-var movement = Vector2()
+export (int) var movement = Vector2(0, 0)
 
 func get_inputs():
 	if Input.is_key_pressed(KEY_H):
 		movement = Vector2(100, 0)
 	else:
-		movement = Vector2()
+		movement = Vector2(0, 0)
 		
 func _process(delta):
 	get_inputs()
 	
 func _physics_process(delta):
-	add_force(Vector2(), movement)
-	pass
+	set_applied_force(movement)
+	
