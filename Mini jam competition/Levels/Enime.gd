5be01daa110e0acbec35b3c7d8083514421245a3
NexInfinite <stormparardiseonline@gmail.com>
2018-07-14 12:27:07 +0100

    Version 3.4 , Movement working. Jump bugged

--- Mini jam competition/Levels/Enime.gd
+++ Mini jam competition/Levels/Enime.gd
@@ -1,2 +1,18 @@
 extends RigidBody2D
 
+var movement = Vector2()
+const MAX_SPEED = 300
+
+func get_inputs():
+	if Input.is_key_pressed(KEY_H):
+		movement = Vector2(MAX_SPEED, 0)
+	else:
+		movement = Vector2()
+		
+func _process(delta):
+	get_inputs()
+	
+func _physics_process(delta):
+	applied_force = movement
+	print(applied_force)
+	
--- Mini jam competition/PLayer/Player.gd
+++ Mini jam competition/PLayer/Player.gd
@@ -2,10 +2,16 @@ extends RigidBody2D
 
 var movement = Vector2()
 const MAX_SPEED = 300
+const JUMP_HEIGHT = 200
+const UP = Vector2(0, -1)
 
 func get_inputs():
 	if Input.is_key_pressed(KEY_H):
 		movement = Vector2(MAX_SPEED, 0)
+	elif Input.is_key_pressed(KEY_F):
+		movement = Vector2(-MAX_SPEED, 0)
+	elif Input.is_key_pressed(KEY_T):
+		print('nein')
 	else:
 		movement = Vector2()
 		
@@ -13,6 +19,7 @@ func _process(delta):
 	get_inputs()
 	
 func _physics_process(delta): 
+	var collisions = get_colliding_bodies()
+	print(collisions)
 	applied_force = movement
-	print(applied_force)
 	
